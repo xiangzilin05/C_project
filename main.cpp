775c8c76b77bb92ce4ea6efb6f8c06fcc7afe0a8
xiangzilin05 <738448954@qq.com>
2017-07-16 16:07:00 +0800

    10

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "10/stock00.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+    Stock kate, joy;
+    kate.show();
+    joy.show();
     return 0;
 }
