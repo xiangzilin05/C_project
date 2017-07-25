5073a2fc1be283a6f42c16319a73d835f75b8ecc
xiangzilin05 <738448954@qq.com>
2017-07-25 17:49:43 +0800

    13

--- 13/tabtenn0/tabtenn0.cpp
+++ 13/tabtenn0/tabtenn0.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 
 TableTennisPlayer::TableTennisPlayer(const string &fn,
-                                     const string &ln, bool ht) : firstname(fn),
-                                                                  lastname(ln), hasTable(ht) {}
+                                     const string &ln, bool ht)
+        : firstname(fn),
+          lastname(ln),
+          hasTable(ht) {}
 
 void TableTennisPlayer::Name() const {
     std::cout << lastname << ", " << firstname;
 }
 
-RatedPlayer::RatedPlayer(unsigned int r, const string &fn, const string &ln, bool ht)
+RatedPlayer::RatedPlayer(unsigned int r, const string &fn,
+                         const string &ln, bool ht)
         : TableTennisPlayer(fn, ln, ht) {
     rating = r;
 }
